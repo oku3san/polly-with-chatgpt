2749df0b2f5f6e60dab4126f68e83b2d6c60fec7
oku3san <takaya0303@gmail.com>
2023-03-12 23:53:58 +0900

    Update

--- src/polly-with-chatgpt/main.go
+++ src/polly-with-chatgpt/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+    "context"
+    "fmt"
+    openai "github.com/sashabaranov/go-openai"
+    "os"
+)
 
 func main() {
-    fmt.Println("initial")
+    token := os.Getenv("OPENAI_API_KEY")
+    client := openai.NewClient(token)
+    resp, err := client.CreateChatCompletion(
+        context.Background(),
+        openai.ChatCompletionRequest{
+            Model: openai.GPT3Dot5Turbo,
+            Messages: []openai.ChatCompletionMessage{
+                {
+                    Role:    openai.ChatMessageRoleUser,
+                    Content: "Hello!",
+                },
+            },
+        },
+    )
+
+    if err != nil {
+        fmt.Printf("ChatCompletion error: %v\n", err)
+        return
+    }
+
+    fmt.Println(resp.Choices[0].Message.Content)
 }
